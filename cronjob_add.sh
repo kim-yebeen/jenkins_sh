b59fed15db12e4d253c996d26a0fc8ecce118ca0
dev-library <96283221+dev-library@users.noreply.github.com>
2024-07-22 15:21:23 +0900

    Update cronjob_add.sh

--- cronjob_add.sh
+++ cronjob_add.sh
@@ -3,6 +3,11 @@
 # 크론탭에서 제거할 구문
 CRON_JOB="* * * * * sudo /var/lib/jenkins/workspace/Jenkins-log-pipeline/resource_usage.sh"
 
+# 현재 작업경로 확인
+echo "---------"
+pwd
+echo "---------"
+
 # 현재 사용자 크론탭을 임시 파일에 백업
 crontab -l > current_crontab.txt
 
